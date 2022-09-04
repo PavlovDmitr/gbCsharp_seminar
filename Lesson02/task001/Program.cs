bc979ebc2180103051c14bb4c43fe4b888f82135
Dmitriy <dfeeler@yandex.ru>
2022-09-04 22:10:39 +0500

    edit les2task3

--- Lesson02/task001/Program.cs
+++ Lesson02/task001/Program.cs
@@ -2,7 +2,7 @@
 // трёхзначное число и удаляет вторую цифру этого
 // числа.
 //for (int i=1; i<=10; i++){
-int num = new Random().Next(10, 100);
+int num = new Random().Next(100, 1000);
 int a1 = num /10;
 int a2 = num % 10;
 if (a1>a2)
---
+++ Lesson02_hw/task03_z15/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 15: Напишите программу, которая принимает на
+// вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+int saturdayNum = 6;
+int sundayNum = 7;
+bool numOk = true;
+
+Console.WriteLine("Введите число - номер дня недели:");
+while (numOk)
+{
+int number = int.Parse(Console.ReadLine());
+if (number >= 1 && number <=7) 
+{
+    if (number == saturdayNum || number == sundayNum) 
+    {Console.WriteLine($"День недели с номером {number}  - выходной"); numOk =false;}
+    else 
+    {Console.WriteLine($"День недели с номером {number}  - НЕ выходной"); numOk =false;}
+}
+else {Console.WriteLine("Номер за пределами нумерации дней недели, попробуйте еще раз:");}
+}
