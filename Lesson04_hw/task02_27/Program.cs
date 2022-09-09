a9391db6d795bf4059177a304d68f99c482d376a
Dmitriy <dfeeler@yandex.ru>
2022-09-10 01:43:21 +0500

    les04hw_t2_27_adjustment

--- Lesson04_hw/task02_27/Program.cs
+++ Lesson04_hw/task02_27/Program.cs
@@ -18,16 +18,17 @@ int num = 0;
 string insStr = "";
 string exitWord = "NO";
 bool checkInsNum = false;
-bool flagExit = true;
-while(flagExit) // просто бесконечная программа, есть ли какой то более удобный и правильный выход из такой программы? ну или может более правильное ее оформление?
+bool flagExit = false; // пока просматривал код подумал что нагляднее когда флаг имеет состояние соответвующее названию,
+                            // если я его назвал "флаг выхода" - то и выходить по нему тогда, когда он true. подумал я)
+while(!flagExit) // просто бесконечная программа, есть ли какой то более удобный и правильный выход из такой программы? ну или может более правильное ее оформление?
 {
     do
     {
         Console.WriteLine("Введите целое число для получения суммы цифр, или введите NO для выхода:");
         insStr = Console.ReadLine();
-        if (insStr==exitWord) {flagExit = false; break;}
+        if (insStr==exitWord) {flagExit = true; break;} 
         checkInsNum = int.TryParse(insStr, out num);
     }
     while (!checkInsNum);
-    if(flagExit) Console.WriteLine($"{num} -> {SumOfDigit(num)}");
+    if(!flagExit) Console.WriteLine($"{num} -> {SumOfDigit(num)}");
 }
