865dbf6adb04e4a8db77220963d22a3d2d5bbb8e
Dmitriy <dfeeler@yandex.ru>
2022-09-10 01:27:03 +0500

    les04hw_t1_25_adjustment

--- Lesson04_hw/task01_25/Program.cs
+++ Lesson04_hw/task01_25/Program.cs
@@ -8,20 +8,21 @@ double Exponentiation(double a, int b)
     double altA= a;
     for(int i=1;i<b;i++)
     a=a*altA;
-    //Console.WriteLine($"согласно условию задания надо цикл рузультат вот - {a}");
     return a;
 }
 
 char[] delimiterChar = {' ',',','.','(',')','[',']','/'};
-Console.WriteLine("Введите число для возведения в степень и степень через пробел:");
-string[] ins = Console.ReadLine().Split(delimiterChar);  
-bool checkNum1 = double.TryParse(ins[0], out double baseA);
-bool checkNum2 = int.TryParse(ins[1], out int expB);
-while (!checkNum1 || !checkNum2)
-{
+string[] ins = new string[5];  
+bool checkNum1 = true;
+bool checkNum2 = true;
+double baseA=0.0;
+int expB = 0;
+do
+{   // Console.WriteLine("постарался скукожить ввод с клавиатуры");
     Console.WriteLine("Введите число для возведения в степень и степень корректно(например \"6 5\" без кавычек):");
     ins = Console.ReadLine().Split(delimiterChar); 
     checkNum1 = double.TryParse(ins[0], out baseA);
     checkNum2 = int.TryParse(ins[1], out expB);
 }
+while (!checkNum1 || !checkNum2);
 Console.WriteLine($"{baseA}, {expB} -> {Exponentiation(baseA,expB)}");
