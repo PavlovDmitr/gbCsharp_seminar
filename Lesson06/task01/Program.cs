c4b3a6b1c244cb3ea0fb8ca286c653c0b8d1bc89
Dmitriy <dfeeler@yandex.ru>
2022-09-17 17:02:22 +0500

    add Les06_Les06

---
+++ Lesson06/task01/Program.cs
@@ -0,0 +1,104 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+void PrintArray(int[] resPA)
+{
+    int size = resPA.Length;
+    Console.Write("[");
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(i != size-1 ? $"{resPA[i]}, ": $"{resPA[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] GetConvertArray(int[] resGCEN)
+{
+    int count = 0;
+    for (int i = 0; i < resGCEN.Length /2 ; i++)
+        (resGCEN[i], resGCEN[resGCEN.Length-i-1]) = (resGCEN[resGCEN.Length-i-1], resGCEN[i]);
+            count++;
+    return count;
+}
+
+string descriptionApp = @"Программа создает массив заданой длины 
+случайными числами и выводит прямую и перевернутую версию";
+int minValueArr = 0, maxValueArr = 10;
+
+Console.Clear();
+Console.WriteLine(descriptionApp);
+Console.WriteLine();
+char[] delimiterChar = { ' ', ',', '.', '(', ')', '[', ']', '/' };
+string[] arrayStrings = new string[2];
+int[] arrayInt = new int[1];
+int arrayLenght = 0;
+bool checkParse = false;
+
+
+do
+{
+    Console.WriteLine("Введите размер массива:");
+    arrayStrings = Console.ReadLine().Split(delimiterChar);
+    if (arrayStrings.Length < 1) {continue;};
+    checkParse = int.TryParse(arrayStrings[0], out arrayLenght);
+}
+while (!checkParse);
+arrayInt = GetArray(arrayLenght, minValueArr, maxValueArr);
+Console.WriteLine($"Массив размерности {arrayLenght} и результат:");
+PrintArray(arrayInt);
+Console.WriteLine($" -> {GetCountEvenNumbers(arrayInt)}"); 
+
+
+
+Console.Clear();
+int [] array = GetArray(10,0,10);
+Console.WriteLine(String.Join(" ",array));
+
+int [] reversArray = ReversArray2(array);
+Console.WriteLine(String.Join(" ",reversArray));
+
+ReversArray1(array);
+Console.WriteLine(String.Join(" ",array));
+
+
+int [] GetArray(int size, int minValue, int maxValue)
+{
+    int [] res = new int[size];
+    for (int i = 0; i<size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue+1);
+
+    }
+    return res;
+}
+
+void ReversArray1(int[] inArray)
+{
+    for(int i=0; i<inArray.Length/2; i++)
+    {
+        int k = inArray[i];
+        inArray[i] = inArray[inArray.Length - i -1];
+        inArray[inArray.Length-i -1] = k;
+    }
+}
+
+int [] ReversArray2(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for(int i=0;i<inArray.Length;i++ )
+    {
+        result[i] = inArray[inArray.Length - 1 -i];
+    }
+    return result;
+}
---
+++ Lesson06_hw/task01_41/Program.cs
@@ -0,0 +1,82 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+bool CheckInsert(List<bool> check)
+{
+    for (int i = 0; i < check.Count; i++)
+    {
+        if (check[i] == false) { return false; }
+    }
+    return true;
+}
+
+int CalcPosNumbers(List<double> numbers)
+{
+    int result = 0;
+    foreach (double number in numbers)
+    {
+        if (number > 0)
+        result++;
+    }
+    return result;
+}
+
+string descriptionApp =
+@"************************************************
+* Программа производит подсчет кол-ва          *
+* введнных пользователем положительных чисел   *
+************************************************";
+string welcomText = @"Введите новое число или
+end  - для окончания ввода,
+quit - для выхода из программы.";
+// Console.Clear();
+// Console.WriteLine(descriptionApp);
+// Console.WriteLine();
+char[] delimiterChar = { ' ', ',', '.', '(', ')', '[', ']', '/' };
+string[]? insArrayStrings = new string[1];
+double[] arrayDouble = new double[1];
+int arrayLenght = 0;
+bool checkParse = false;
+// do
+// {
+//     Console.Clear();
+//     Console.WriteLine(descriptionApp);
+//     Console.WriteLine();
+//     Console.WriteLine(welcomText);
+//     insArrayStrings = Console.ReadLine().Split(delimiterChar);
+//     // if(insArrayStrings[0].ToLower() == "quit"|| insArrayStrings[0].ToLower() == "end") break;
+//     if (insArrayStrings.Length < 1) continue;
+//     for(int i=0; i<insArrayStrings.Length; i++)
+//     checkParse = int.TryParse(insArrayStrings[i], out arrayDouble[i]);
+// }
+// while (!checkParse);
+// // if(insArrayStrings[0].ToLower() == "quit") System.Environment.Exit(0);
+// Console.ReadLine();
+
+List<double> numberList = new List<double> { };
+List<bool> checkList = new List<bool> { };
+Console.WriteLine("Введите любое кол-во чисел через пробел,");
+
+do
+{
+    string? insStr = Console.ReadLine();
+    insArrayStrings = insStr?.Split(delimiterChar);
+    for (int i = 0; i < insArrayStrings?.Length; i++)
+    {
+        checkParse =  double.TryParse(insArrayStrings[i], out double nextNum);
+        checkList.Add(checkParse);
+        //Console.WriteLine(check[i]);
+        numberList.Add(nextNum);
+    }
+    Console.WriteLine("Пробуем разобрать что написано...");
+    if (CheckInsert(checkList))
+    {
+        Console.WriteLine($"{CalcPosNumbers(numberList)} ");
+
+    }
+    else
+    {
+        Console.WriteLine($"Введите нормальные числа - для дробных чисел разделитель целой и дробной части в Windows запятая!");
+    }
+} while (!(CheckInsert(checkList)));
