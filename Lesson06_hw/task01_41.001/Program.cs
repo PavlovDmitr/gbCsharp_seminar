79f9d900f6bc8d6a2f1e79259761cf135094b7f1
Dmitriy <dfeeler@yandex.ru>
2022-09-20 01:37:47 +0500

    les06hw_add41.001Fstruct

---
+++ Lesson06_hw/task01_41.001/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using System;
+
+namespace task01_41 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static string descriptionApp =
+@"************************************************
+* Программа производит подсчет кол-ва          *
+* введнных пользователем положительных чисел   *
+************************************************";
+        static bool CheckInsert(bool[] check)
+        {
+            for (int i = 0; i < check.Length; i++)
+            {
+                if (check[i] == false) { return false; }
+            }
+            return true;
+        }
+
+        static int CalcPosNumbers(double[] numbers)
+        {
+            int result = 0;
+            foreach (double number in numbers)
+            {
+                if (number > 0)
+                result++;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine(descriptionApp);
+            Console.WriteLine();
+            bool[] checkParse = new bool[1];
+            double[] arrayDouble = new double[1];
+            char[] delimiterChar = { ' ', '.', '(', ')', '[', ']', '/'};
+            string[]? insArrayStrings = new string[1];
+            Console.WriteLine("Введите любое кол-во чисел через пробел:");
+
+            do
+            {
+                string? insStr = Console.ReadLine().Trim(delimiterChar);
+                insArrayStrings = insStr?.Split(delimiterChar);
+                int len = insArrayStrings.Length;
+                arrayDouble = new double[len];
+                checkParse = new bool[len];
+                
+                for (int i = 0; i < len; i++)
+                {
+                    checkParse[i] =  double.TryParse(insArrayStrings[i], out arrayDouble[i]);
+                }
+                
+                if (CheckInsert(checkParse))
+                {
+                    Console.WriteLine("Результат работы программы:");
+                    Console.WriteLine($"[{String.Join(", ",arrayDouble)}] -> {CalcPosNumbers(arrayDouble)} ");
+                }
+                else
+                {
+                    Console.WriteLine($"Введите нормальные числа - для дробных чисел разделитель целой и дробной части в Windows запятая!");
+                }
+            } while (!(CheckInsert(checkParse)));
+        }
+    }
+}
+
+
+
+
+
+
